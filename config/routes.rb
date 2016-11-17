42a0327f870028a1f40306387b168842224d4436
Keerthi Siva <keerthisiv@gmail.com>
2016-11-18 03:25:31 +0530

    working backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   match '/tasks', to: 'tasks#create', via: [:post, :options]
+   match '/task_complete', to: 'tasks#complete', via: [:post, :options]
+   match '/tasks/:id', to: 'tasks#destroy', via: [:delete, :options]
 end
